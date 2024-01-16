6ef5223c3d3f182f416d6613ede831148bf76830
horek <azenoxe@gmail.com>
2024-01-16 15:56:44 +0100

    feat: add next-matomo to project with env variables for environements

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -4,13 +4,29 @@ import DefaultLayout from "~/layouts/DefaultLayout";
 import "~/styles/globals.css";
 
 import { api } from "~/utils/api";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { theme } from "~/utils/chakra-theme";
 import { usePathname } from "next/navigation";
 import { AuthProvider } from "~/providers/Auth";
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_TRACKING_ENABLED = process.env
+  .NEXT_PUBLIC_MATOMO_TRACKING_ENABLED as string;
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL as string;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID as string;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const pathname = usePathname();
+
+  useEffect(() => {
+    if (MATOMO_TRACKING_ENABLED === "true") {
+      init({
+        url: MATOMO_URL,
+        siteId: MATOMO_SITE_ID,
+      });
+    }
+  }, []);
+
   const getLayout = (children: ReactNode) => {
     if (pathname.startsWith("/admin")) {
       return children;
