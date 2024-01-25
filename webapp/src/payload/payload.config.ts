2205fb81305019ef76cdfc556ec74752e2116ce6
horek <azenoxe@gmail.com>
2024-01-25 09:53:21 +0100

    fix: database connection string

--- webapp/src/payload/payload.config.ts
+++ webapp/src/payload/payload.config.ts
@@ -32,7 +32,7 @@ export default buildConfig({
       connectionString:
         process.env.NODE_ENV !== "production"
           ? process.env.DATABASE_URL
-          : `postgresql://${process.env.PGDATABASE}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?sslmode=disable`,
+          : `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?sslmode=disable`,
     },
   }),
   plugins: [
