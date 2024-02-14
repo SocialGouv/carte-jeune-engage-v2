dc3b8edbc7b2243da2c6458066c3ff2678357f2f
horek <azenoxe@gmail.com>
2024-02-14 11:35:40 +0100

    fix: reorganize signup page and add form tag for native validation

--- webapp/src/pages/signup.tsx
+++ webapp/src/pages/signup.tsx
@@ -35,12 +35,91 @@ export type SignUpFormStep = {
   field: FieldProps;
 };
 
-export default function Home() {
+const signupSteps = [
+  {
+    title: "Bienvenue ! Comment peut-on vous appeler ?",
+    field: {
+      name: "civility",
+      kind: "text",
+      label: "Civilité",
+      rules: {
+        required: "Ce champ est obligatoire",
+      },
+    },
+  },
+  {
+    title: "Quel est votre prénom ?",
+    field: {
+      name: "firstName",
+      kind: "text",
+      label: "Prénom",
+      rules: {
+        required: "Ce champ est obligatoire",
+      },
+    },
+  },
+  {
+    title: "Quel est votre nom de famille ?",
+    field: {
+      name: "lastName",
+      kind: "text",
+      label: "Nom de famille",
+      rules: {
+        required: "Ce champ est obligatoire",
+      },
+    },
+  },
+  {
+    title: "Quelle est votre date de naissance ?",
+    description:
+      "Votre date de naissance nous permet de personnaliser votre expérience et de garantir la sécurité de vos données",
+    field: {
+      name: "birthDate",
+      kind: "date",
+      label: "Date de naissance",
+      rules: {
+        required: "Ce champ est obligatoire",
+      },
+    },
+  },
+  {
+    title: "Quelle est votre adresse email ?",
+    description:
+      "Votre adresse email vous permettra aussi de vous reconnecter si vous changez de numéro de téléphone par exemple",
+    field: {
+      name: "email",
+      kind: "email",
+      label: "Email",
+      rules: {
+        required: "Ce champ est obligatoire",
+        email: "Veuillez saisir une adresse email valide",
+      },
+    },
+  },
+  {
+    title: "Votre adresse",
+    description:
+      "Votre adresse nous permet de chercher les meilleures réductions en magasin",
+    field: {
+      name: "address",
+      kind: "text",
+      label: "Adresse",
+      rules: {
+        required: "Ce champ est obligatoire",
+      },
+    },
+  },
+] as const;
+
+export default function Signup() {
   const router = useRouter();
 
   const { signupStep } = router.query as {
     signupStep: keyof SignUpForm | undefined;
   };
+
+  const [currentSignupStep, setCurrentSignupStep] =
+    useState<SignUpFormStep | null>(null);
 
   const defaultValues = useMemo(() => {
     return typeof window !== "undefined"
@@ -48,19 +127,17 @@ export default function Home() {
       : {};
   }, [typeof window !== "undefined"]);
 
-  const { handleSubmit, register, getValues, watch, control, formState } =
-    useForm<SignUpForm>({
-      mode: "all",
-      defaultValues,
-    });
-
-  const formValues = watch();
-
-  useEffect(() => {
-    localStorage.setItem("cje-signup-form", JSON.stringify(formValues));
-  }, [formValues]);
-
-  const { errors } = formState;
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<SignUpForm>({
+    mode: "all",
+    defaultValues,
+  });
 
   const onSubmit: SubmitHandler<SignUpForm> = (data) => {
     if (!currentSignupStep) return;
@@ -77,84 +154,11 @@ export default function Home() {
     }
   };
 
-  const signupSteps = [
-    {
-      title: "Bienvenue ! Comment peut-on vous appeler ?",
-      field: {
-        name: "civility",
-        kind: "text",
-        label: "Civilité",
-        rules: {
-          required: "Ce champ est obligatoire",
-        },
-      },
-    },
-    {
-      title: "Quel est votre prénom ?",
-      field: {
-        name: "firstName",
-        kind: "text",
-        label: "Prénom",
-        rules: {
-          required: "Ce champ est obligatoire",
-        },
-      },
-    },
-    {
-      title: "Quel est votre nom de famille ?",
-      field: {
-        name: "lastName",
-        kind: "text",
-        label: "Nom de famille",
-        rules: {
-          required: "Ce champ est obligatoire",
-        },
-      },
-    },
-    {
-      title: "Quelle est votre date de naissance ?",
-      description:
-        "Votre date de naissance nous permet de personnaliser votre expérience et de garantir la sécurité de vos données",
-      field: {
-        name: "birthDate",
-        kind: "date",
-        label: "Date de naissance",
-        rules: {
-          required: "Ce champ est obligatoire",
-        },
-      },
-    },
-    {
-      title: "Quelle est votre adresse email ?",
-      description:
-        "Votre adresse email vous permettra aussi de vous reconnecter si vous changez de numéro de téléphone par exemple",
-      field: {
-        name: "email",
-        kind: "email",
-        label: "Email",
-        rules: {
-          required: "Ce champ est obligatoire",
-          email: "Veuillez saisir une adresse email valide",
-        },
-      },
-    },
-    {
-      title: "Votre adresse",
-      description:
-        "Votre adresse nous permet de chercher les meilleures réductions en magasin",
-      field: {
-        name: "address",
-        kind: "text",
-        label: "Adresse",
-        rules: {
-          required: "Ce champ est obligatoire",
-        },
-      },
-    },
-  ] as const;
+  const formValues = watch();
 
-  const [currentSignupStep, setCurrentSignupStep] =
-    useState<SignUpFormStep | null>(null);
+  useEffect(() => {
+    localStorage.setItem("cje-signup-form", JSON.stringify(formValues));
+  }, [formValues]);
 
   useEffect(() => {
     if (!signupStep || typeof signupStep !== "string") {
@@ -189,73 +193,69 @@ export default function Home() {
   );
 
   return (
-    <Flex
-      display="flex"
-      flexDir="column"
-      py={12}
-      px={6}
-      justifyContent="space-between"
-      h="full"
-    >
-      <Flex flexDir="column" justifyContent="center">
-        <Heading as="h1" size="md" fontWeight="extrabold" mb={4}>
-          {currentSignupStep?.title}
-        </Heading>
-        <Text fontSize="sm" fontWeight="medium" color="secondaryText">
-          {currentSignupStep?.description ||
-            "Saisissez la même information que sur vos documents administratifs officiels."}
-        </Text>
-        <Box mt={6} key={currentSignupStep.field.name}>
-          {currentSignupStep.field.name === "civility" ? (
-            <Flex alignItems="center" w="full" gap={6}>
-              <Controller
-                control={control}
-                name="civility"
-                render={({ field: { onChange, value } }) => (
-                  <>
-                    <FormBlock
-                      value="man"
-                      currentValue={value}
-                      onChange={onChange}
-                    >
-                      Monsieur
-                    </FormBlock>
-                    <FormBlock
-                      value="woman"
-                      currentValue={value}
-                      onChange={onChange}
-                    >
-                      Madame
-                    </FormBlock>
-                  </>
-                )}
-              />
-            </Flex>
-          ) : (
-            <FormInput
-              register={register}
-              field={currentSignupStep.field}
-              fieldError={
-                errors[currentSignupStep?.field.name as keyof SignUpForm]
-              }
-            />
-          )}
-        </Box>
-      </Flex>
-      <Button
-        colorScheme="blackBtn"
-        isDisabled={!currentFieldValue || currentFieldValue === ""}
-        type={
-          currentSignupStep.field.name ===
-          signupSteps[signupSteps.length - 1]?.field.name
-            ? "submit"
-            : "button"
-        }
-        rightIcon={<Icon as={HiArrowRight} w={6} h={6} />}
-        onClick={handleSubmit(onSubmit)}
+    <form onSubmit={handleSubmit(onSubmit)} style={{ height: "100%" }}>
+      <Flex
+        display="flex"
+        flexDir="column"
+        py={12}
+        px={6}
+        justifyContent="space-between"
+        h="full"
       >
-        Continuer
-      </Button>
-    </Flex>
+        <Flex flexDir="column" justifyContent="center">
+          <Heading as="h1" size="md" fontWeight="extrabold" mb={4}>
+            {currentSignupStep?.title}
+          </Heading>
+          <Text fontSize="sm" fontWeight="medium" color="secondaryText">
+            {currentSignupStep?.description ||
+              "Saisissez la même information que sur vos documents administratifs officiels."}
+          </Text>
+          <Box mt={6} key={currentSignupStep.field.name}>
+            {currentSignupStep.field.name === "civility" ? (
+              <Flex alignItems="center" w="full" gap={6}>
+                <Controller
+                  control={control}
+                  name={currentSignupStep.field.name}
+                  render={({ field: { onChange, value } }) => (
+                    <>
+                      <FormBlock
+                        value="man"
+                        currentValue={value}
+                        onChange={onChange}
+                      >
+                        Monsieur
+                      </FormBlock>
+                      <FormBlock
+                        value="woman"
+                        currentValue={value}
+                        onChange={onChange}
+                      >
+                        Madame
+                      </FormBlock>
+                    </>
+                  )}
+                />
+              </Flex>
+            ) : (
+              <FormInput
+                register={register}
+                field={currentSignupStep.field}
+                fieldError={
+                  errors[currentSignupStep?.field.name as keyof SignUpForm]
+                }
+              />
+            )}
+          </Box>
+        </Flex>
+        <Button
+          colorScheme="blackBtn"
+          isDisabled={!currentFieldValue || currentFieldValue === ""}
+          type="submit"
+          rightIcon={<Icon as={HiArrowRight} w={6} h={6} />}
+        >
+          Continuer
+        </Button>
+      </Flex>
+    </form>
   );
 }
