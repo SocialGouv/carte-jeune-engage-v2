818e10134e889320163155d5a7ff0fedf766ee34
horek <azenoxe@gmail.com>
2024-03-11 10:42:28 +0100

    feat: add burger navbar for mobile view

--- webapp/src/components/landing/Header.tsx
+++ webapp/src/components/landing/Header.tsx
@@ -1,19 +1,19 @@
 import {
   Box,
   Button,
-  Divider,
+  Collapse,
   Flex,
   Icon,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
+  Portal,
+  Stack,
   Text,
   useBreakpointValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import ChakraNextImage from "../ChakraNextImage";
-import { HiMiniBars3 } from "react-icons/hi2";
+import { HiMiniBars3, HiXMark } from "react-icons/hi2";
 import Link from "next/link";
+import { useRef } from "react";
 
 export const menuItems = [
   { title: "Qu'est-ce que c'est ?", slug: "what-is-it" },
@@ -24,79 +24,137 @@ export const menuItems = [
 
 const Header = () => {
   const isDesktop = useBreakpointValue({ base: false, lg: true });
+  const { isOpen, onToggle } = useDisclosure();
+
+  const headerRef = useRef<HTMLDivElement>(null);
 
   const handleIsEligibleClick = () => {
     const element = document.querySelector(".phone-number-cta");
     if (element) (element as HTMLElement).focus();
+    onToggle();
   };
 
   return (
-    <Flex
-      id="login-gov-image"
-      position="sticky"
-      top={0}
-      zIndex={1000}
-      py={6}
-      pb={4}
-      bgColor="white"
-      justifyContent="space-between"
-      alignItems="center"
-      pl={4}
-      pr={6}
-    >
-      <Flex id="login-gov-image" alignItems="center" h="full">
-        <ChakraNextImage
-          src="/images/marianne.svg"
-          alt="Logo marianne du gouvernement français"
-          width={74}
-          height={49}
-          borderRight="1px solid"
-          borderColor="#808080"
-          pr={2}
-          mr={3}
-        />
-        <ChakraNextImage
-          src="/images/cje-logo.png"
-          alt="Logo de l'application Carte Jeune Engagé"
-          width={69}
-          height={38}
-        />
+    <Box ref={headerRef} position="sticky" top={0} zIndex={1000}>
+      <Flex
+        id="login-gov-image"
+        py={6}
+        pb={4}
+        bgColor="white"
+        justifyContent="space-between"
+        alignItems="center"
+        pl={4}
+        pr={6}
+      >
+        <Flex id="login-gov-image" alignItems="center" h="full">
+          <ChakraNextImage
+            src="/images/marianne.svg"
+            alt="Logo marianne du gouvernement français"
+            width={74}
+            height={49}
+            borderRight="1px solid"
+            borderColor="#808080"
+            pr={2}
+            mr={3}
+          />
+          <ChakraNextImage
+            src="/images/cje-logo.png"
+            alt="Logo de l'application Carte Jeune Engagé"
+            width={69}
+            height={38}
+          />
+        </Flex>
+        {!isDesktop ? (
+          <Stack ml="auto">
+            <Icon
+              as={!isOpen ? HiMiniBars3 : HiXMark}
+              w={8}
+              h={8}
+              onClick={onToggle}
+              cursor="pointer"
+              display={{ md: "none" }}
+            />
+            <Portal containerRef={headerRef}>
+              <Collapse in={isOpen}>
+                <Stack
+                  p={4}
+                  display={{ md: "none" }}
+                  position="absolute"
+                  top={20}
+                  bgColor="white"
+                  w="full"
+                >
+                  {menuItems.map(({ slug, title }, index) => (
+                    <Stack
+                      key={slug}
+                      textAlign="center"
+                      bgColor="bgWhite"
+                      borderTopRadius={index === 0 ? "2xl" : "none"}
+                      borderBottom="1px solid"
+                      borderColor="gray.300"
+                    >
+                      <Box
+                        py={3}
+                        as="a"
+                        href={`/#${slug}-section` ?? "#"}
+                        onClick={onToggle}
+                        justifyContent="space-between"
+                        alignItems="center"
+                        _hover={{
+                          textDecoration: "none",
+                        }}
+                      >
+                        <Text fontWeight={600} color="gray.700">
+                          {title}
+                        </Text>
+                      </Box>
+                    </Stack>
+                  ))}
+                  <Stack
+                    textAlign="center"
+                    bgColor="bgWhite"
+                    borderColor="gray.300"
+                    borderBottomRadius="2xl"
+                    py={3}
+                    px={8}
+                  >
+                    <Button
+                      colorScheme="blackBtn"
+                      onClick={handleIsEligibleClick}
+                      size="md"
+                      py={7}
+                      borderRadius="3xl"
+                    >
+                      Vérifier mon éligibilité
+                    </Button>
+                  </Stack>
+                </Stack>
+              </Collapse>
+            </Portal>
+          </Stack>
+        ) : (
+          <>
+            <Flex alignItems="center" gap={8}>
+              {menuItems.map((item) => (
+                <Link key={item.slug} href={`/#${item.slug}-section`}>
+                  <Text fontWeight="medium">{item.title}</Text>
+                </Link>
+              ))}
+            </Flex>
+            <Button
+              size="md"
+              py={7}
+              px={5}
+              borderRadius="xl"
+              fontSize="lg"
+              onClick={handleIsEligibleClick}
+            >
+              Vérifier mon éligibilité
+            </Button>
+          </>
+        )}
       </Flex>
-      {!isDesktop ? (
-        <Menu>
-          <MenuButton>
-            <Icon as={HiMiniBars3} w={8} h={8} />
-          </MenuButton>
-          <MenuList style={{ position: "relative" }}>
-            {menuItems.map((item) => (
-              <Link key={item.slug} href={`/#${item.slug}-section`}>
-                <MenuItem key={item.title}>{item.title}</MenuItem>
-              </Link>
-            ))}
-          </MenuList>
-        </Menu>
-      ) : (
-        <>
-          <Flex alignItems="center" gap={8}>
-            {menuItems.map((item) => (
-              <Link key={item.slug} href={`/#${item.slug}-section`}>
-                <Text fontWeight="medium">{item.title}</Text>
-              </Link>
-            ))}
-          </Flex>
-          <Button
-            size="md"
-            py={7}
-            px={5}
-            borderRadius="xl"
-            fontSize="lg"
-            onClick={handleIsEligibleClick}
-          >
-            Vérifier mon éligibilité
-          </Button>
-        </>
-      )}
-    </Flex>
+    </Box>
   );
 };
 
