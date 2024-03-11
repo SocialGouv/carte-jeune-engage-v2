990c07933b362a39e7e0a3f81942d3f520e4bc97
Clément <clement@numericite.eu>
2024-03-11 19:10:49 +0100

    fix: loader center horizontally

--- webapp/src/components/BigLoader.tsx
+++ webapp/src/components/BigLoader.tsx
@@ -1,17 +1,17 @@
 import { Flex, Spinner } from "@chakra-ui/react";
 
 const BigLoader = () => {
-  return (
-    <Flex h="100vh" w="100vw" justifyContent={"center"} alignItems={"center"}>
-      <Spinner
-        thickness="5px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="blackLight"
-        boxSize={24}
-      />
-    </Flex>
-  );
+	return (
+		<Flex h="100vh" w="100%" justifyContent={"center"} alignItems={"center"}>
+			<Spinner
+				thickness="5px"
+				speed="0.65s"
+				emptyColor="gray.200"
+				color="blackLight"
+				boxSize={24}
+			/>
+		</Flex>
+	);
 };
 
 export default BigLoader;
