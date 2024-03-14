66c531dbff625be23c4b0811e1fd3bdabaf73045
Clément <clement@numericite.eu>
2024-03-14 16:38:55 +0100

    feat: sms signup & new octopush account

--- webapp/src/server/api/routers/permission.ts
+++ webapp/src/server/api/routers/permission.ts
@@ -13,10 +13,13 @@ export const permissionRouter = createTRPCRouter({
 			})
 		)
 		.mutation(async ({ ctx, input }) => {
+			const { phone_number } = input;
+			const hasDialingCode = phone_number.startsWith("+")
+
 			const existing = await ctx.payload.find({
 				collection: "permissions",
 				where: {
-					phone_number: { equals: input.phone_number }
+					phone_number: { equals: phone_number }
 				},
 				page: 1,
 				limit: 1
@@ -34,9 +37,37 @@ export const permissionRouter = createTRPCRouter({
 				const permission = await ctx.payload.create({
 					collection: "permissions",
 					data: {
-						...input,
+						phone_number
 					},
 				});
+
+				const octopushResponse = await fetch(
+					"https://api.octopush.com/v1/public/sms-campaign/send",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							"api-login": process.env.OCTOPUSH_API_LOGIN as string,
+							"api-key": process.env.OCTOPUSH_API_KEY as string,
+							'cache-control': 'no-cache'
+						},
+						body: JSON.stringify({
+							recipients: [
+								{
+									phone_number: hasDialingCode
+										? phone_number
+										: `+33${phone_number.substring(1)}`
+								}
+							],
+							text: `
+								Vous avez le droit aux réductions de la carte « jeune engagé » ! Pour en bénéficier télécharger l’application sur ce lien : https://cje.fabrique.social.gouv.fr/. STOP au 30101
+							`,
+						}),
+					}
+				)
+					.then((response) => response.json())
+					.then((data) => data);
+				console.log(octopushResponse)
 
 				return {
 					data: permission,
