0d7f62201cba1eab050de6883844855cab5e3d71
Clément Lelong <clement@numericite.eu>
2024-03-16 11:12:03 +0100

    fix: migration

--- webapp/src/payload/migrations/20240316_084545.ts
+++ webapp/src/payload/migrations/20240316_084545.ts
@@ -1,25 +1,12 @@
-import { MigrateUpArgs, MigrateDownArgs } from '@payloadcms/db-postgres'
-import { sql } from 'drizzle-orm'
+import { MigrateUpArgs, MigrateDownArgs } from "@payloadcms/db-postgres";
+import { sql } from "drizzle-orm";
 
 export async function up({ payload }: MigrateUpArgs): Promise<void> {
-await payload.db.drizzle.execute(sql`
-
-DO $$ BEGIN
- CREATE TYPE "enum_offers_barcode_format" AS ENUM('CODE39', 'EAN13', 'ITF14', 'MSI', 'pharmacode', 'codabar', 'upc');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
-
-ALTER TABLE "supervisors" ADD COLUMN "cgu" boolean;
-ALTER TABLE "offers" ADD COLUMN "barcodeFormat" "enum_offers_barcode_format";`);
-
-};
+  await payload.db.drizzle.execute(sql`
+ALTER TABLE "supervisors" ADD COLUMN "cgu" boolean;`);
+}
 
 export async function down({ payload }: MigrateDownArgs): Promise<void> {
-await payload.db.drizzle.execute(sql`
-
-ALTER TYPE "enum_users_status_image" ADD VALUE 'rejected';
-ALTER TABLE "supervisors" DROP COLUMN IF EXISTS "cgu";
-ALTER TABLE "offers" DROP COLUMN IF EXISTS "barcodeFormat";`);
-
-};
+  await payload.db.drizzle.execute(sql`
+ALTER TABLE "supervisors" DROP COLUMN IF EXISTS "cgu";`);
+}
