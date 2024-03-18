d4e694543ddacd1c91755ec4e42a6258c47ef005
Clément <clement@numericite.eu>
2024-03-18 13:55:29 +0100

    fix: preprod url mail

--- webapp/src/utils/tools.ts
+++ webapp/src/utils/tools.ts
@@ -3,113 +3,113 @@ import crypto from "crypto";
 import { Where } from "payload/types";
 
 export const convertFrenchDateToEnglish = (
-  frenchDate: string
+	frenchDate: string
 ): string | null => {
-  const match = frenchDate.match(/^(\d{2})\/(\d{2})\/(\d{2})$/);
+	const match = frenchDate.match(/^(\d{2})\/(\d{2})\/(\d{2})$/);
 
-  if (match) {
-    const [, day, month, year] = match.map(Number);
+	if (match) {
+		const [, day, month, year] = match.map(Number);
 
-    if (year !== undefined && month !== undefined && day !== undefined) {
-      // Creating a Date object with a four-digit year
-      const englishFormattedDate = new Date(2000 + year, month - 1, day);
+		if (year !== undefined && month !== undefined && day !== undefined) {
+			// Creating a Date object with a four-digit year
+			const englishFormattedDate = new Date(2000 + year, month - 1, day);
 
-      // Using toISOString to get the date in ISO format (YYYY-MM-DD)
-      return englishFormattedDate.toISOString().split("T")[0] || null;
-    }
-  }
+			// Using toISOString to get the date in ISO format (YYYY-MM-DD)
+			return englishFormattedDate.toISOString().split("T")[0] || null;
+		}
+	}
 
-  // Return null if the input format is incorrect
-  return null;
+	// Return null if the input format is incorrect
+	return null;
 };
 
 export const frenchPhoneNumber = /^(?:\+33[1-9](\d{8})|(?!.*\+\d{2}).{10})$/;
 
 export const payloadOrPhoneNumberCheck = (phone_number: string): Where => {
-  const hasDialingCode = phone_number.startsWith("+");
+	const hasDialingCode = phone_number.startsWith("+");
 
-  return {
-    or: [
-      {
-        phone_number: {
-          equals: hasDialingCode
-            ? phone_number
-            : `+33${phone_number.substring(1)}`,
-        },
-      },
-      {
-        phone_number: {
-          equals: hasDialingCode
-            ? `0${phone_number.substring(3)}`
-            : phone_number,
-        },
-      },
-    ],
-  };
+	return {
+		or: [
+			{
+				phone_number: {
+					equals: hasDialingCode
+						? phone_number
+						: `+33${phone_number.substring(1)}`,
+				},
+			},
+			{
+				phone_number: {
+					equals: hasDialingCode
+						? `0${phone_number.substring(3)}`
+						: phone_number,
+				},
+			},
+		],
+	};
 };
 
 export const payloadWhereOfferIsValid = (prefix?: string): Where => {
-  const nowDate = new Date().toISOString().split("T")[0] as string;
-  let finalPrefix = prefix ? `${prefix}.` : "";
+	const nowDate = new Date().toISOString().split("T")[0] as string;
+	let finalPrefix = prefix ? `${prefix}.` : "";
 
-  return {
-    and: [
-      {
-        [`${finalPrefix}validityTo`]: {
-          greater_than_equal: `${nowDate}T00:00:00.000`,
-        },
-      },
-      {
-        or: [
-          {
-            [`${finalPrefix}validityFrom`]: {
-              exists: false,
-            },
-          },
-          {
-            [`${finalPrefix}validityFrom`]: {
-              less_than_equal: `${nowDate}T23:59:59.000`,
-            },
-          },
-        ],
-      },
-    ],
-  };
+	return {
+		and: [
+			{
+				[`${finalPrefix}validityTo`]: {
+					greater_than_equal: `${nowDate}T00:00:00.000`,
+				},
+			},
+			{
+				or: [
+					{
+						[`${finalPrefix}validityFrom`]: {
+							exists: false,
+						},
+					},
+					{
+						[`${finalPrefix}validityFrom`]: {
+							less_than_equal: `${nowDate}T23:59:59.000`,
+						},
+					},
+				],
+			},
+		],
+	};
 };
 
 export const generateRandomCode = (): string => {
-  const min = 1000;
-  const max = 9999;
-  const randomCode = Math.floor(Math.random() * (max - min + 1) + min);
-  return randomCode.toString();
+	const min = 1000;
+	const max = 9999;
+	const randomCode = Math.floor(Math.random() * (max - min + 1) + min);
+	return randomCode.toString();
 };
 
 export const generateRandomPassword = (length: number): string => {
-  const buffer = crypto.randomBytes(length);
-  const password = buffer
-    .toString("base64")
-    .replace(/[/+=]/g, "")
-    .slice(0, length);
+	const buffer = crypto.randomBytes(length);
+	const password = buffer
+		.toString("base64")
+		.replace(/[/+=]/g, "")
+		.slice(0, length);
 
-  return password;
+	return password;
 };
 
 export const addSpaceToTwoCharacters = (input: string) => {
-  var result = input.replace(/(\d{2})/g, "$1 ");
+	var result = input.replace(/(\d{2})/g, "$1 ");
 
-  result = result.trim();
+	result = result.trim();
 
-  return result;
+	return result;
 };
 
 export const getBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_ENV_APP === "production")
-    return "https://cje.fabrique.social.gouv.fr";
-  if (process.env.NEXT_PUBLIC_ENV_APP === "preproduction")
-    return "https://cje-preprod.fabrique.social.gouv.fr";
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+	if (process.env.NEXT_PUBLIC_ENV_APP === "production")
+		return "https://cje.fabrique.social.gouv.fr";
+	if (process.env.NEXT_PUBLIC_ENV_APP === "preproduction")
+		return "https://cje-preprod.ovh.fabrique.social.gouv.fr/";
+	return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
 export const isIOS = () => {
-  return !!navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+	return !!navigator.userAgent.match(/(iPod|iPhone|iPad)/);
 };
