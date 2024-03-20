417706c10fd1131b1b95d93d87828f9ba8b08591
Clément <clement@numericite.eu>
2024-03-20 17:56:02 +0100

    fix: new QR code ios issue

--- webapp/src/components/landing/QRCode.tsx
+++ webapp/src/components/landing/QRCode.tsx
@@ -1,4 +1,4 @@
-import { Box, ChakraProps } from "@chakra-ui/react";
+import { Box, ChakraProps, Image } from "@chakra-ui/react";
 import { QRCode } from "react-qrcode-logo";
 import { getBaseUrl } from "~/utils/tools";
 
@@ -11,16 +11,22 @@ const QRCodeWrapper = ({ size, wrapperProps }: QRCodeProps) => {
 	// REMOVE LOGO FOR NOW - ISSUES WITH SCANNING ON SOME PHONES
 	return (
 		<Box {...wrapperProps} borderRadius="2xl">
+			<Image src="/images/landing/qr-code-prod.png"
+				alt="QR Code production"
+				h="200px"
+				w="200px"
+			/>
+			{/* REMOVE FOR NOW , ISSUES ON IPHONE
 			<QRCode
 				value={getBaseUrl()}
 				qrStyle="dots"
-				// logoWidth={50}
-				// logoHeight={30}
+				logoWidth={50}
+				logoHeight={30}
 				eyeRadius={16}
-				// logoPadding={2.5}
+				logoPadding={2.5}
 				size={size ?? 256}
-			// logoImage="/images/cje-logo.png"
-			/>
+			logoImage="/images/cje-logo.png"
+			/> */}
 		</Box>
 	);
 };
