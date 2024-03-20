f4b7830631997a8d68a6b1f81fa6e3cecd2f45fc
Clément <clement@numericite.eu>
2024-03-20 16:06:36 +0100

    fix: hide QR code logo

--- webapp/src/components/landing/QRCode.tsx
+++ webapp/src/components/landing/QRCode.tsx
@@ -3,25 +3,26 @@ import { QRCode } from "react-qrcode-logo";
 import { getBaseUrl } from "~/utils/tools";
 
 type QRCodeProps = {
-  size?: number;
-  wrapperProps?: ChakraProps;
+	size?: number;
+	wrapperProps?: ChakraProps;
 };
 
 const QRCodeWrapper = ({ size, wrapperProps }: QRCodeProps) => {
-  return (
-    <Box {...wrapperProps} borderRadius="2xl">
-      <QRCode
-        value={getBaseUrl()}
-        qrStyle="dots"
-        logoWidth={50}
-        logoHeight={30}
-        eyeRadius={16}
-        logoPadding={2.5}
-        size={size ?? 256}
-        logoImage="/images/cje-logo.png"
-      />
-    </Box>
-  );
+	// REMOVE LOGO FOR NOW - ISSUES WITH SCANNING ON SOME PHONES
+	return (
+		<Box {...wrapperProps} borderRadius="2xl">
+			<QRCode
+				value={getBaseUrl()}
+				qrStyle="dots"
+				// logoWidth={50}
+				// logoHeight={30}
+				eyeRadius={16}
+				// logoPadding={2.5}
+				size={size ?? 256}
+			// logoImage="/images/cje-logo.png"
+			/>
+		</Box>
+	);
 };
 
 export default QRCodeWrapper;
