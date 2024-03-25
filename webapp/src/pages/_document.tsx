4d132ff605792d92efae57cb4489d7f86998d036
Clément <clement@numericite.eu>
2024-03-25 18:25:26 +0100

    wip: tarteaucitron integration

--- webapp/src/pages/_document.tsx
+++ webapp/src/pages/_document.tsx
@@ -1,13 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head >
+				<script
+					type="text/javascript"
+					src="/static/tarteaucitron/tarteaucitron.js"
+				/>
+				<script
+					type="text/javascript"
+					src="/static/tarteaucitron/initTarteaucitron.js"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
