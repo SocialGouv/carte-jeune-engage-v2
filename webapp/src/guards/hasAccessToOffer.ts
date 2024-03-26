fb4cf0321e995f5bf72e32434d00158276ad09f6
Clément Lelong <clement@numericite.eu>
2024-03-26 09:41:12 +0100

    fix: increase nbSeen only on not activated offers

--- webapp/src/guards/hasAccessToOffer.ts
+++ webapp/src/guards/hasAccessToOffer.ts
@@ -5,44 +5,52 @@ import { PayloadJwtSession, createCallerFactory } from "~/server/api/trpc";
 import { jwtDecode } from "jwt-decode";
 
 export const hasAccessToOffer: GetServerSideProps = async (context) => {
-	const payload = await getPayloadClient({ seed: false });
+  const payload = await getPayloadClient({ seed: false });
 
-	const jwtCookie =
-		context.req.cookies[process.env.NEXT_PUBLIC_JWT_NAME ?? "cje-jwt"];
+  const jwtCookie =
+    context.req.cookies[process.env.NEXT_PUBLIC_JWT_NAME ?? "cje-jwt"];
 
-	if (!jwtCookie) {
-		return {
-			redirect: {
-				destination: "/",
-				permanent: false,
-			},
-		};
-	}
+  if (!jwtCookie) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
-	const session = jwtDecode<PayloadJwtSession>(jwtCookie);
+  const session = jwtDecode<PayloadJwtSession>(jwtCookie);
 
-	const createCaller = createCallerFactory(appRouter);
+  const createCaller = createCallerFactory(appRouter);
 
-	const caller = createCaller({ payload, session });
+  const caller = createCaller({ payload, session });
 
-	const { data: offerListAvailables } = await caller.offer.getListOfAvailables({
-		offerId: parseInt(context.params?.id as string),
-		page: 1,
-		perPage: 1,
-	});
+  const { data: offerListAvailables } = await caller.offer.getListOfAvailables({
+    offerId: parseInt(context.params?.id as string),
+    page: 1,
+    perPage: 1,
+  });
 
-	if (offerListAvailables?.length === 0) {
-		return {
-			redirect: {
-				destination: "/",
-				permanent: false,
-			},
-		};
-	}
+  if (offerListAvailables?.length === 0) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
-	await caller.offer.increaseNbSeen({ offer_id: parseInt(context.params?.id as string) })
+  const { data: myCoupon } = await caller.coupon.getOne({
+    offer_id: parseInt(context.params?.id as string),
+  });
 
-	return {
-		props: {},
-	};
+  if (!myCoupon) {
+    await caller.offer.increaseNbSeen({
+      offer_id: parseInt(context.params?.id as string),
+    });
+  }
+
+  return {
+    props: {},
+  };
 };
