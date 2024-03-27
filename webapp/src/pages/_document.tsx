cd3489d2c96ba8f869abbcd7858d754894768209
Clément <clement@numericite.eu>
2024-03-27 12:49:17 +0100

    feat: load different hotjar id by env

--- webapp/src/pages/_document.tsx
+++ webapp/src/pages/_document.tsx
@@ -2,6 +2,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
 export default function Document() {
+	console.log(process.env.NEXT_PUBLIC_ENV_APP)
+
+	const getTarteAuCitronInitByEnv = () => {
+		switch (process.env.NEXT_PUBLIC_ENV_APP) {
+			case "preproduction":
+				return <script
+					type="text/javascript"
+					src="/static/tarteaucitron/env/preprod/initTarteaucitron.js"
+				/>
+			case "production":
+				return <script
+					type="text/javascript"
+					src="/static/tarteaucitron/env/prod/initTarteaucitron.js"
+				/>
+		}
+	}
 	return (
 		<Html lang="en">
 			<Head >
@@ -9,10 +25,7 @@ export default function Document() {
 					type="text/javascript"
 					src="/static/tarteaucitron/tarteaucitron.js"
 				/>
-				<script
-					type="text/javascript"
-					src="/static/tarteaucitron/initTarteaucitron.js"
-				/>
+				{getTarteAuCitronInitByEnv()}
 			</Head>
 			<body>
 				<Main />
