04de78999d2fecfb17112c6287d29e7fe53624da
Clément <clement@numericite.eu>
2024-03-27 14:34:30 +0100

    fix: wording no activated offers

--- webapp/src/pages/dashboard/wallet/index.tsx
+++ webapp/src/pages/dashboard/wallet/index.tsx
@@ -1,11 +1,11 @@
 import {
-  Box,
-  Center,
-  Flex,
-  Heading,
-  Icon,
-  TabPanel,
-  Text,
+	Box,
+	Center,
+	Flex,
+	Heading,
+	Icon,
+	TabPanel,
+	Text,
 } from "@chakra-ui/react";
 import LoadingLoader from "~/components/LoadingLoader";
 import OfferCard from "~/components/cards/OfferCard";
@@ -18,109 +18,106 @@ import { Offer } from "~/payload/payload-types";
 import { push } from "@socialgouv/matomo-next";
 
 const WalletNoData = ({ kind }: { kind: Offer["kind"] }) => {
-  return (
-    <Box textAlign="center" mt={40}>
-      <Icon as={PiSmileySadFill} w={10} h={10} />
-      <Heading size="md" mt={1} mb={2}>
-        Aucune offre actuellement ici
-      </Heading>
-      <Text color="gray.500">
-        Activez les {kind === "voucher" ? "bons de réductions" : "codes promos"}{" "}
-        qui vous intéressent il s’afficheront ici pour que vous puissiez les
-        utiliser
-      </Text>
-    </Box>
-  );
+	return (
+		<Box textAlign="center" mt={40} px={8}>
+			<Heading size="md" mt={1} mb={2}>
+				Commencez à activer des offres pour les retrouver ici !
+			</Heading>
+			<Text color="gray.500">
+				Activez les offres qui vous intéressent celles à utiliser {kind === "voucher" ? "en magasin 🛒" : "en ligne 🛍️"} vont s’afficher ici
+			</Text>
+		</Box>
+	);
 };
 
 export default function Wallet() {
-  const router = useRouter();
-  const { offerKind } = router.query as {
-    offerKind?: Offer["kind"];
-  };
+	const router = useRouter();
+	const { offerKind } = router.query as {
+		offerKind?: Offer["kind"];
+	};
 
-  const [tabIndex, setTabIndex] = useState(2);
+	const [tabIndex, setTabIndex] = useState(2);
 
-  useEffect(() => {
-    setTabIndex(offerKind === "voucher" ? 0 : offerKind === "code" ? 1 : 2);
-  }, [offerKind]);
+	useEffect(() => {
+		setTabIndex(offerKind === "voucher" ? 0 : offerKind === "code" ? 1 : 2);
+	}, [offerKind]);
 
-  const handleTabsChange = (index: number) => {
-    router.replace({
-      query: { offerKind: index === 0 ? "voucher" : "code" },
-    });
-    push([
-      "trackEvent",
-      "Mes réductions",
-      `${index === 0 ? "En magasin" : "En ligne"}`,
-    ]);
-    setTabIndex(index);
-  };
+	const handleTabsChange = (index: number) => {
+		router.replace({
+			query: { offerKind: index === 0 ? "voucher" : "code" },
+		});
+		push([
+			"trackEvent",
+			"Mes réductions",
+			`${index === 0 ? "En magasin" : "En ligne"}`,
+		]);
+		setTabIndex(index);
+	};
 
-  if (
-    (!offerKind || (offerKind !== "voucher" && offerKind !== "code")) &&
-    router.isReady
-  ) {
-    router.replace({
-      query: {
-        ...router.query,
-        offerKind: "voucher",
-      },
-    });
-  }
+	if (
+		(!offerKind || (offerKind !== "voucher" && offerKind !== "code")) &&
+		router.isReady
+	) {
+		router.replace({
+			query: {
+				...router.query,
+				offerKind: "voucher",
+			},
+		});
+	}
 
-  const { data: resultUserOffers, isLoading: isLoadingUserOffers } =
-    api.offer.getListOfAvailables.useQuery({
-      page: 1,
-      perPage: 50,
-      sort: "partner.name",
-      isCurrentUser: true,
-    });
+	const { data: resultUserOffers, isLoading: isLoadingUserOffers } =
+		api.offer.getListOfAvailables.useQuery({
+			page: 1,
+			perPage: 50,
+			sort: "partner.name",
+			isCurrentUser: true,
+		});
 
-  const { data: currentUserOffers } = resultUserOffers || {};
+	const { data: currentUserOffers } = resultUserOffers || {};
 
-  const inStoreOffers = currentUserOffers?.filter((offer) =>
-    offer.kind.startsWith("voucher")
-  );
+	const inStoreOffers = currentUserOffers?.filter((offer) =>
+		offer.kind.startsWith("voucher")
+	);
 
-  const onlineOffers = currentUserOffers?.filter((offer) =>
-    offer.kind.startsWith("code")
-  );
+	const onlineOffers = currentUserOffers?.filter((offer) =>
+		offer.kind.startsWith("code")
+	);
 
-  if (isLoadingUserOffers) {
-    return (
-      <WalletWrapper tabIndex={tabIndex} handleTabsChange={handleTabsChange}>
-        <Center h="full" w="full">
-          <LoadingLoader />
-        </Center>
-      </WalletWrapper>
-    );
-  }
+	if (isLoadingUserOffers) {
+		return (
+			<WalletWrapper tabIndex={tabIndex} handleTabsChange={handleTabsChange}>
+				<Center h="full" w="full">
+					<LoadingLoader />
+				</Center>
+			</WalletWrapper>
+		);
+	}
 
-  return (
-    <WalletWrapper tabIndex={tabIndex} handleTabsChange={handleTabsChange}>
-      <TabPanel p={0}>
-        {inStoreOffers && inStoreOffers.length > 0 ? (
-          <Flex flexDir="column" gap={6}>
-            {inStoreOffers?.map((offer) => (
-              <OfferCard key={offer.id} offer={offer} displayExpiryDate />
-            ))}
-          </Flex>
-        ) : (
-          <WalletNoData kind="voucher" />
-        )}
-      </TabPanel>
-      <TabPanel p={0}>
-        {onlineOffers && onlineOffers.length > 0 ? (
-          <Flex flexDir="column" gap={6}>
-            {onlineOffers?.map((offer) => (
-              <OfferCard key={offer.id} offer={offer} displayExpiryDate />
-            ))}
-          </Flex>
-        ) : (
-          <WalletNoData kind="code" />
-        )}
-      </TabPanel>
-    </WalletWrapper>
-  );
+	return (
+		<WalletWrapper tabIndex={tabIndex} handleTabsChange={handleTabsChange}>
+			<TabPanel p={0}>
+				{inStoreOffers && inStoreOffers.length > 0 ? (
+					<Flex flexDir="column" gap={6}>
+						{inStoreOffers?.map((offer) => (
+							<OfferCard key={offer.id} offer={offer} displayExpiryDate />
+						))}
+					</Flex>
+				) : (
+					<WalletNoData kind="voucher" />
+				)}
+			</TabPanel>
+			<TabPanel p={0}>
+				{onlineOffers && onlineOffers.length > 0 ? (
+					<Flex flexDir="column" gap={6}>
+						{onlineOffers?.map((offer) => (
+							<OfferCard key={offer.id} offer={offer} displayExpiryDate />
+						))}
+					</Flex>
+				) : (
+					<WalletNoData kind="code" />
+				)}
+			</TabPanel>
+		</WalletWrapper>
+	);
 }
